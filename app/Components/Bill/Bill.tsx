3d505ac4c64a0ed2d09f1169f22a4baf5e57c029
Сергей <nochnoy_briz@mail.ru>
2025-01-20 18:27:57 +0300

    first

--- app/Components/Bill/Bill.tsx
+++ app/Components/Bill/Bill.tsx
@@ -3,15 +3,15 @@ import styles from './Bill.module.css'
 import clsx from "clsx";
 const Bill:FC = () => {
     return (
-        // <div className={styles.wrap}>
-            <div className={styles.sheme}>
-                <div className={styles.circle}>
-                    <div className={clsx(styles.circle,styles.circle2)}>
+        <div className={styles.wrap}>
+            {/*<div className={styles.sheme}>*/}
+            {/*    <div className={styles.circle}>*/}
+            {/*        <div className={clsx(styles.circle,styles.circle2)}>*/}
 
-                    </div>
-                </div>
-            </div>
-        // </div>
+            {/*        </div>*/}
+            {/*    </div>*/}
+            {/*</div>*/}
+        </div>
     )
 };
 export default Bill;
